c75dffa67716028bd1c0e98e24cdc40f06242527
MossDog <lukejohnmosshughes@gmail.com>
2025-11-28 22:58:43 +0000

    Fix: Variable Shadowing Variable names in constructor signature no longer shadow field names.

--- Classes/Level.gd
+++ Classes/Level.gd
@@ -9,9 +9,9 @@ var title: String
 var path: String
 var preview: Texture2D
 
-func _init(id: int = 0, chapter_id: int = 0, title: String = "", path: String = "", preview: Texture2D = null):
-	self.id = id
-	self.chapter_id = chapter_id
-	self.title = title
-	self.path = path
-	self.preview = preview
+func _init(lvl_id: int = 0, lvl_chptr_id: int = 0, lvl_title: String = "", lvl_path: String = "", lvl_preview: Texture2D = null):
+	self.id = lvl_id
+	self.chapter_id = lvl_chptr_id
+	self.title = lvl_title
+	self.path = lvl_path
+	self.preview = lvl_preview

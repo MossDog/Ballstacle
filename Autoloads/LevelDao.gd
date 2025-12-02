3e9b7b27b98ef4079b67338f4cdf3d0af2f60414
MossDog <lukejohnmosshughes@gmail.com>
2025-12-02 15:37:57 +0000

    Level data load simplified

--- Autoloads/LevelDao.gd
+++ Autoloads/LevelDao.gd
@@ -1,21 +1,17 @@
 extends Node
-class_name LevelDao
 
 var levels: Array[Level]
-const LEVEL_DIR = "res://Scenes/Levels"
+const LEVEL_DIR = "res://Scenes/Levels/"
 
 func load_levels():
 	var dir := DirAccess.open(LEVEL_DIR)
 	for file_name in dir.get_files():
 		if file_name.begins_with("Level"):
-			var id = get_id_from_path(file_name)
-			var chapter_id = 0 # PLACEHOLDER
-			var title = "" # PLACEHOLDER
+			print("FILE NAME ", file_name)
+			var data = file_name.split("_")
+			print(data)
+			var id = int(data[2])
+			var chapter_id = int(data[1])
+			var title = data[3]
 			var path = LEVEL_DIR + file_name
 			levels.append(Level.new(id, chapter_id, title, path, null))
-				
-func get_id_from_path(path: String) -> int:
-	var regex := RegEx.new()
-	regex.compile("\\d+")
-	var id = int(regex.search(path).get_string())
-	return id
